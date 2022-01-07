c038e316c2fdffbb3608b6afd1f7d71271a5fb1b
arash-sadeghi <arash.sadeghi1997@gmail.com>
2022-01-07 18:03:38 +0000

    weather tempreture added to video

--- rosbag2videao.py
+++ rosbag2videao.py
@@ -8,6 +8,20 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from time import ctime, time
 
+import csv,datetime
+
+def format_number(n):
+    if n<10:
+        return '0'+str(n)
+    else:
+        return str(n)
+def get_temp(csv_content,second):
+    date=datetime.datetime.strptime(ctime(second),"%a %b %d %H:%M:%S %Y")
+    date_format=format_number(date.year)+'-'+format_number(date.month)+'-'+format_number(date.day)
+    for row in csv_content:
+        if row[0]==date_format:
+            return "tmin: "+row[2]+" tavg: "+row[1]+" tmax: "+row[3]
+    return "temp_not_found"
 class Args:
     def __init__(self):
         self.bag_file=""
@@ -42,7 +56,15 @@ if __name__ == '__main__':
     size = (1280,720)
     FPS=48
     video=cv2.VideoWriter(video_file_name,cv2.VideoWriter_fourcc(*'XVID'), FPS, size)
-    try:
+    weather_file_name="graz_weather_dec.csv"
+    csv_file=open(weather_file_name)
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_content=[]
+    for i in csv_reader:
+        csv_content.append(i)
+    csv_file.close()
+    #try:
+    if True:
         for file in bag_list:
             print("[+] Extract images from {} on topic {} into {}".format(file,args.image_topic, video_file_name))
             bag_log.write(args.input_dir+file+'\n')
@@ -57,6 +79,9 @@ if __name__ == '__main__':
                     cv_img = bridge.compressed_imgmsg_to_cv2(msg)
                     cv_img=cv2.cvtColor(cv_img, cv2.COLOR_GRAY2BGR)
                     cv2.putText(cv_img,ctime(t.to_sec()) , (0,size[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                    temp_str=get_temp(csv_content,t.to_sec())
+                    cv2.putText(cv_img,temp_str , (size[0]//2,size[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0, 0), 2, cv2.LINE_AA)
+                    # cv2.imwrite("im.png",cv_img)
                     video.write(cv_img)
                     count += 1
                     percentage=count/(24*3600/30)*100
@@ -68,10 +93,11 @@ if __name__ == '__main__':
         video.release()
         print("\n[+] DONE!")
     
-    except Exception as E:
-        print('[-] Error occured: ',E)
-        bag_log.close()
-        video.release()
+   #  except Exception as E:
+   #     print('[-] Error occured: ',E)
+   #     bag_log.close()
+   #     video.release()
+   #     csv_file.close()
 ''' ctime(t.to_sec()) '''
 '''
 30 mins one day
